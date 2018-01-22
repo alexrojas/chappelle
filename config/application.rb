069cf701a9552519a55269f34e971efbe29a01b9
alexrojas <alex.rojas@agintechllc.com>
2018-01-22 16:43:32 -0500

    add config.assets.precompile += %w(vendor/assets/images/*)

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,8 @@ module DavidApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.serve_static_assets = true
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.assets.precompile += %w(/assets/images/*)
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
